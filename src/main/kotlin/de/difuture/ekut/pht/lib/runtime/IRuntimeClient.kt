d19b65ee55928e3d7b7b33f8ac687916daf715a6
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-08-06 16:16:42 +0200

    Prepare IRuntimeClient for release 0.0.1

---
+++ src/main/kotlin/de/difuture/ekut/pht/lib/runtime/IRuntimeClient.kt
@@ -0,0 +1,16 @@
+package de.difuture.ekut.pht.lib.runtime
+
+
+/**
+ * Top-most interface of runtime clients implemented by the station.
+ *
+ * Represents the Client that will run trains at the station. Implementers of this interface use
+ * and depend on the resources offered by the infrastructure of the station. As an example, the
+ * [IDockerClient] depends on the host system offering a running Docker daemon and the user being
+ * able to communicate with the daemon.
+ *
+ * @author Lukas Zimmermann
+ * @since 0.0.1
+ *
+ */
+interface IRuntimeClient : AutoCloseable
