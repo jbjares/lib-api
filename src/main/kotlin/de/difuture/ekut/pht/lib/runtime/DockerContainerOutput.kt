84ab4e06e764cb34cef023cdac4f2a05d47c3885
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-08-07 12:32:33 +0200

    Fix: Add missing DockerContainerOutput class

---
+++ src/main/kotlin/de/difuture/ekut/pht/lib/runtime/DockerContainerOutput.kt
@@ -0,0 +1,33 @@
+package de.difuture.ekut.pht.lib.runtime
+
+import de.difuture.ekut.pht.lib.common.docker.DockerContainerId
+
+
+/**
+ * Represents the output of a Docker Container after it has been exited.
+ *
+ * @author Lukas Zimmermann
+ */
+data class DockerContainerOutput(
+
+        /**
+         * The ID of the container that has been started
+         */
+        val containerId: DockerContainerId,
+
+        /**
+         * The exit code of the exited container
+         */
+        val exitCode: Int,
+
+        /**
+         * Standard output of the container, represented as [String]
+         *
+         */
+        val stdout : String,
+
+        /**
+         * Standard error of the container, represented as [String]
+         */
+        val stderr : String
+)
