bf2cdda1f2d7f7581347b4ef810b464df914cc28
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-08-23 15:54:08 +0200

    Add: StationPing

---
+++ src/test/kotlin/de/difuture/ekut/pht/lib/station/StationPingTests.kt
@@ -0,0 +1,31 @@
+package de.difuture.ekut.pht.lib.station
+
+import de.difuture.ekut.pht.lib.allEqualAfterSerialization
+import io.github.benas.randombeans.api.EnhancedRandom
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Tests [StationPing]
+ *
+ * @author Lukas Zimmermann
+ * @see [StationPing]
+ * @since 0.0.1
+ *
+ */
+class StationPingTests {
+
+    private lateinit var events: List<StationPing>
+
+    @Before
+    fun before() {
+
+        this.events = EnhancedRandom.randomListOf(30, StationPing::class.java)
+    }
+
+    @Test
+    fun testSerialization() {
+
+        allEqualAfterSerialization(this.events, StationPing::class.java)
+    }
+}
